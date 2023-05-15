0de37d6eeb86b6ad9166735bb6a413bc7b6bb6bc
lucapl <lukasz.andryszewski@outlook.com>
2023-05-15 18:05:26 +0200

    Documented the TextDecorator

--- src/main/java/pl/put/poznan/transformer/logic/decorator/TextDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorator/TextDecorator.java
@@ -2,13 +2,30 @@ package pl.put.poznan.transformer.logic.decorator;
 
 import pl.put.poznan.transformer.logic.TextConverter;
 
+/**
+ * Wrapper for the TextConverter class
+ */
 public abstract class TextDecorator implements TextConverter {
+
+    /**
+     * Wrapped text converter
+     */
     protected TextConverter wrappee;
 
+    /**
+     * Creates an instance of the class, with another text converter inside
+     * @param wrappee a text converter which is part of the decorator pipeline
+     */
     public TextDecorator(TextConverter wrappee){
         this.wrappee = wrappee;
     }
 
+    /**
+     * Converts the method according to the converter type.
+     * Additionally, runs this method on the wrappee.
+     * @param text input
+     * @return text converter by all the wrappees
+     */
     @Override
     public String convert(String text){
         return wrappee.convert(text);
