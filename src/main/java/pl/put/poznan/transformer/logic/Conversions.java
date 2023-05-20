32f176c5d24244ade1632a4d07af28896d640662
lucapl <lukasz.andryszewski@outlook.com>
2023-05-20 13:33:54 +0200

    Enumerated possible conversions

---
+++ src/main/java/pl/put/poznan/transformer/logic/Conversions.java
@@ -0,0 +1,15 @@
+package pl.put.poznan.transformer.logic;
+
+public enum Conversions {
+    CASE_UPPER,
+    CASE_LOWER,
+    CASE_CAPITAL,
+    REVERSE,
+    REVERSE_PRESERVE_CASE,
+    ACRONYMISE,
+    ACRONYMS_UNWIND,
+    LATEX,
+    NUMS_INTS,
+    NUMS_REAL,
+    REMOVE_REPEATS
+}
