a091656a48b5f65a7f0fcdd5752e0850d4029f29
lucapl <lukasz.andryszewski@outlook.com>
2023-05-20 13:29:00 +0200

    TextTransformer documentation

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -1,16 +1,28 @@
 package pl.put.poznan.transformer.logic;
 
 /**
- * This is just an example to show that the logic should be outside the REST service.
+ * Controls the program logic, creates the decorated converters based on the transforms
  */
 public class TextTransformer {
 
+    /**
+     * The transformations to be performed based on the request
+     */
     private final String[] transforms;
 
+    /**
+     * Creates the transformer
+     * @param transforms to be performed on text
+     */
     public TextTransformer(String[] transforms){
         this.transforms = transforms;
     }
 
+    /**
+     * Transforms the text
+     * @param text initial text
+     * @return transformed text
+     */
     public String transform(String text){
         // of course, normally it would do something based on the transforms
         return text.toUpperCase();
