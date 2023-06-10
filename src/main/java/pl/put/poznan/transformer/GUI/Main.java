ae18d910dc4a8b3d509813782ad188a344d4ea57
ErykWalter <erykwalter2002@gmail.com>
2023-06-10 17:59:06 +0200

    update: changed path to a fxml file

--- src/main/java/pl/put/poznan/transformer/GUI/Main.java
+++ src/main/java/pl/put/poznan/transformer/GUI/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/UI.fxml"));
         primaryStage.setTitle("Text Editor");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
