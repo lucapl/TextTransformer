204bb07fe3dc2b15011e4dce99ace62d7bdfca57
ErykWalter <erykwalter2002@gmail.com>
2023-06-10 12:55:30 +0200

    fix: rename files in GUI package such that they have proper naming

--- src/main/java/pl/put/poznan/transformer/GUI/Main.java
+++ src/main/java/pl/put/poznan/transformer/GUI/Main.java
@@ -7,9 +7,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
         primaryStage.setTitle("Text Editor");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
