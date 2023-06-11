53e57ef4530a67e0df5d672b8930cb4c6e7a4249
ErykWalter <ErykWalter@users.noreply.github.com>
2023-06-11 20:03:42 +0000

    Deploying to javadoc from @ lucapl/TextTransformer@b48fba83e0e72d9c4d68bde666c43e8846c565d6 🚀

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.put.poznan.transformer.app"},{"l":"pl.put.poznan.transformer.logic"},{"l":"pl.put.poznan.transformer.logic.decorator"},{"l":"pl.put.poznan.transformer.rest"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.put.poznan.transformer.app"},{"l":"pl.put.poznan.transformer.GUI"},{"l":"pl.put.poznan.transformer.logic"},{"l":"pl.put.poznan.transformer.logic.decorator"},{"l":"pl.put.poznan.transformer.rest"}];updateSearchResults();
